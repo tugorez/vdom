ed88614659c7a637e97bef23325936439e282f94
tugorez <tugorez@gmail.com>
2019-12-25 13:06:13 -0800

    Add createVApp

--- src/main.js
+++ src/main.js
@@ -2,20 +2,29 @@ import createElement from './vdom/createElement';
 import render from './vdom/render';
 import mount from './vdom/mount';
 
-const vApp = createElement('div', {
-  attrs: {
-    id: 'app',
-  },
-  children: [
-    'Hello World',
-    createElement('img', {
-      attrs: {
-        src: 'https://media.giphy.com/media/cuPm4p4pClZVC/giphy.gif',
-      },
-    }),
-  ],
-});
+const createVApp = count =>
+  createElement('div', {
+    attrs: {
+      id: 'app',
+      dataCount: count,
+    },
+    children: [
+      'The current count is: ',
+      String(count),
+      createElement('img', {
+        attrs: {
+          src: 'https://media.giphy.com/media/cuPm4p4pClZVC/giphy.gif',
+        },
+      }),
+    ],
+  });
 
+let count = 0;
+const vApp = createVApp(count);
 const $app = render(vApp);
+let $rootEl = mount($app, document.getElementById('app'));
 
-mount($app, document.getElementById('app'));
+setInterval(() => {
+  count++;
+  $rootEl = mount(render(createVApp(count)), $rootEl);
+}, 1000);
